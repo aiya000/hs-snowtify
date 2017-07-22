4aa9d38baec991d785f051912ea2438c8026fc18
aiya000 <aiya000.develop@gmail.com>
2017-07-22 20:57:08 +0900

    Add old implementation

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,42 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Monad (void, forM_, when)
+import Data.List (foldl')
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import Safe (headMay)
+import Turtle (Shell, sh, procStrictWithErr, ExitCode(..), cut, Pattern, newline, spaces, proc)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = sh $ do
+  (exitCode, out, err) <- procStrictWithErr "stack" ["test"] ""
+  let result = out <> err
+  if exitCode == ExitSuccess
+     then void . notifySend $ "stack test is succeed: " <> result
+     else do
+       let blobs = cut sections result
+       notifySend "stack test is finished with errors"
+       forM_ blobs $ \blob ->
+         when (isErrorSection blob) . void $ notifySend blob
+  where
+    sections :: Pattern ()
+    sections = void $ do
+      newline
+      spaces
+      newline
+
+    isErrorSection :: Text -> Bool
+    isErrorSection x =
+      case headMay $ T.lines x of
+        Nothing        -> False
+        Just firstLine -> any (== "error:") $ T.words firstLine
+
+
+-- | Send a message to the notify-daemon
+notifySend :: Text -> Shell ExitCode
+notifySend msg = proc "notify-send" ["snowtify", msg] ""
