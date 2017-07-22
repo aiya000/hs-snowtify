e8120e3bf86668ce31a59cef2808d1ef15bb5740
aiya000 <aiya000.develop@gmail.com>
2017-07-22 21:29:05 +0900

    Implement 'build' and default behavior

--- src/Main.hs
+++ src/Main.hs
@@ -2,27 +2,60 @@
 
 module Main (main) where
 
+import Control.Exception.Safe (MonadThrow, SomeException)
 import Control.Monad (void, forM_, when)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Either (runEitherT)
+import Data.Either (isLeft)
 import Data.List (foldl')
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import Safe (headMay)
-import Turtle (Shell, sh, procStrictWithErr, ExitCode(..), cut, Pattern, newline, spaces, proc)
+import Turtle (Shell, sh, arguments, procStrictWithErr, ExitCode(..), cut, Pattern, newline, spaces, proc)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
 
+-- | Call `execute` with a haskell-stack command
 main :: IO ()
-main = sh $ do
-  (exitCode, out, err) <- procStrictWithErr "stack" ["test"] ""
+main = runEitherT getBuildCommand >>= sh . execute
+  where
+    -- Get a sub command of haskell-stack from CLI arguments.
+    -- This must be "build" or "test", or empty.
+    -- If CLI arguments is not given, return "build".
+    -- If CLI arguments is neither "build" nor "test", throw an exception.
+    getBuildCommand :: (MonadIO m, MonadThrow m) => m Text
+    getBuildCommand = do
+      args <- headMay <$> liftIO arguments
+      case args of
+        Nothing      -> return "build" -- run 'build' by default
+        Just "test"  -> return "test"
+        Just "build" -> return "build"
+        Just unknown -> fail . T.unpack $ "snowtify doesn't know '" <> unknown <> "' command x("
+
+
+execute :: Either SomeException Text -> Shell ()
+execute (Left err)      = liftIO $ print err
+execute (Right command) = do
+  (exitCode, out, err) <- procStrictWithErr "stack" [command] ""
   let result = out <> err
-  if exitCode == ExitSuccess
-     then void . notifySend $ "stack test is succeed: " <> result
-     else do
-       let blobs = cut sections result
-       notifySend "stack test is finished with errors"
-       forM_ blobs $ \blob ->
-         when (isErrorSection blob) . void $ notifySend blob
+  let notifyer = if exitCode == ExitSuccess
+                    then notifySucceeding
+                    else notifyErrors
+  notifyer result
+
+
+-- | Show succeeding with the notify-daemon
+notifySucceeding :: Text -> Shell ()
+notifySucceeding = void . notifySend . ("stack test is succeed: " <>)
+
+-- | Show errors with the notify-daemon
+notifyErrors :: Text -> Shell ()
+notifyErrors result = do
+  let blobs = cut sections result
+  notifySend "stack test is finished with errors"
+  forM_ blobs $ \blob ->
+    when (isErrorSection blob) . void $ notifySend blob
   where
     sections :: Pattern ()
     sections = void $ do
